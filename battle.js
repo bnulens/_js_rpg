db2d2761795651fd4e2c68bf6f7cef587bdc0aab
bnulens <50783895+bnulens@users.noreply.github.com>
2019-08-06 15:18:57 +0200

    initial
    at the moment I work solo

---
+++ battle.js
@@ -0,0 +1,17 @@
+// TIMER for BATTLE
+// var timeleft = 120;
+// var downloadTimer = setInterval(function(){
+//   document.getElementById("countdown").innerText = timeleft;
+//   timeleft -= 1;
+//   if(timeleft <= 0){
+//     clearInterval(downloadTimer);
+//     document.getElementById("countdown").innerText = "Time's up !"
+//   }
+// }, 1000);
+
+import Person from "./character.js";
+
+    let Knight = new Person ("Human",);
+    let Troll = new Person ("Troll", );
+    let Elf = new Person ("Elves", );
+    let Ghoul = new Person ("Ghoul", );
---
+++ character.js
@@ -0,0 +1,24 @@
+//Use this script to generate your character
+export default function Person(race,item){
+    this.race = race;
+    this.item = item;
+    this.currenthealth = 100;
+    this.maxHealth = 100;
+    
+    this.min = 3;
+    this.maxDamage = 20;
+    this.maxHealing = 30;
+
+    this.heal = function(){};
+
+    this.damage = function(){};
+
+    this.totalDamage = this.damage();
+
+    displayChar(this.race,this.item,this.maxHealth);
+}
+
+//Delete this functiom
+function displayChar(race,item,maxHealth){
+    return console.log(`I am a proud member of the ${race} race, I wield a ${item}, my total health point are ${maxHealth}`);
+}

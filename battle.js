70c98a55d379e90b749d03718d8c372e462c2344
Brecht <brechtnulens@gmail.com>
2019-08-06 15:58:45 +0200

    start js

--- battle.js
+++ battle.js
@@ -1,13 +1,6 @@
-// TIMER for BATTLE
-// var timeleft = 120;
-// var downloadTimer = setInterval(function(){
-//   document.getElementById("countdown").innerText = timeleft;
-//   timeleft -= 1;
-//   if(timeleft <= 0){
-//     clearInterval(downloadTimer);
-//     document.getElementById("countdown").innerText = "Time's up !"
-//   }
-// }, 1000);
+let selectionAvatarOne = document.getElementById("player-one-avatar");
+let selectionAvatarTwo = document.getElementById("player-two-avatar");
+
 
 import Person from "./character.js";
 
@@ -15,3 +8,17 @@ import Person from "./character.js";
     let Troll = new Person ("Troll", );
     let Elf = new Person ("Elves", );
     let Ghoul = new Person ("Ghoul", );
+
+let playerOneAttack = Math.floor(Math.random() * Person.maxDamage);
+
+
+/* TIMER for BATTLE
+ var timeleft = 120;
+ var downloadTimer = setInterval(function(){
+   document.getElementById("countdown").innerText = timeleft;
+   timeleft -= 1;
+   if(timeleft <= 0){
+     clearInterval(downloadTimer);
+     document.getElementById("countdown").innerText = "Time's up !"
+    }
+ }, 1000); */

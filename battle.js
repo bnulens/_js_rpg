210e64ecf3af468205b1464b8b7789da898d9ae1
Brecht <brechtnulens@gmail.com>
2019-08-09 10:42:27 +0200

    friday morning

--- battle.js
+++ battle.js
@@ -5,7 +5,7 @@
 // CREATION PAGE SCRIPT
 // 
 // Creating an Array for the various character images
-const warriorArrayV2 = [
+const warriorArray = [
   {
     img: 'images/samurai.png',
     name: 'Hideyoshi',
@@ -48,7 +48,7 @@ const warriorArrayV2 = [
     maxHealth: 120,
     maxDamage: 15,
     maxHealing: 40,
-    maxDeflect: 15
+    maxDeflect: 15,
   }
 ]
 
@@ -59,65 +59,75 @@ const nameHeader = document.createElement('h2');
 let previousP1Warrior = document.getElementById("previous-one");
 let nextP1Warrior = document.getElementById("next-one");
 const p1avatar = document.getElementById("player-one-avatar");
+const p2avatar = document.getElementById("player-two-avatar");
+
 // CREATE STATS 
 let p1stats = document.getElementById("selection-player-one-stats");
 
 
 // Variables of the Main ARRAY
-var warriorClan = warriorArrayV2.length;
-var lastWarrior = warriorArrayV2[warriorClan-1];
+var warriorClan = warriorArray.length;
+var lastWarrior = warriorArray[warriorClan-1];
+var firstWarrior = warriorArray[warriorClan[0]];
 
 // Scroll through Warriors 
 let i = 0;
 
-previousP1Warrior.addEventListener("click", function(){
-  
-  if (i === -1){
-    i = 3;
+document.addEventListener('click', function(e){
+  if (e.target.matches("#previous-one")){
+    getWarriorImage()
+    i -= 1;
+    if (i === -1){
+      i = 3;
+    } 
+  } if (e.target.matches("#next-one")){
+    getWarriorImage()
+    i += 1;
+    if (i === 4){
+      i = 0;
+    } 
   }
+});
 
-  let warrior = warriorArrayV2[i];
+document.addEventListener('click', function(f){
+  if (f.target.matches("#previous-two")){
+    getWarriorImage()
+    i -= 1;
+    if (i === -1){
+      i = 3;
+    }
+  } if (f.target.matches("#next-two")){
+    getWarriorImage()
+    i += 1;
+    if (i === 4){
+      i = 0;
+    } 
+  }
+});
+
+// make function to display image 
+function getWarriorImage(){
+
+  let warrior = warriorArray[i];
+  let warriorSpace = document.createElement("div");
+  warriorSpace.setAttribute("class", "warrior-space");
   let warrior_image = warrior["img"];
-  let warrior_name = warrior["name"];
-  let makeName = document.createTextNode(warrior_name);
-
   createWarriorImage.setAttribute("src", warrior_image);
   createWarriorImage.setAttribute("class", "warrior-image");
-  createWarriorImage.setAttribute("height", "375");
-  createWarriorImage.setAttribute("alt", warrior_name);
-  p1avatar.appendChild(createWarriorImage);
-
-  warriorStats.setAttribute("class", "warrior-stats");
-
-  
-  nameHeader.appendChild(makeName);
-  warriorStats.appendChild(nameHeader);
-  p1stats.appendChild(warriorStats);
-
-  i -= 1;
-
-  return warriorStats;
-})
-
-nextP1Warrior.addEventListener("click", function(){
-
-  if (i === 4){
-    i = 0;
-  }
-
-  let warrior = warriorArrayV2[i];
-  let warrior_image = warrior["img"];
-  createWarriorImage.setAttribute("src", warrior_image);
-  createWarriorImage.setAttribute("class", "warrior-image");
-  createWarriorImage.setAttribute("height", "375");
+  createWarriorImage.setAttribute("height", "425");
   createWarriorImage.setAttribute("alt", "selected warrior");
-  p1avatar.appendChild(createWarriorImage);
+  warriorSpace.appendChild(createWarriorImage);
+  p1avatar.appendChild(warriorSpace);
+  p2avatar.innerHTML = p1avatar.innerHTML;
+}
 
-  i += 1;
-})
+// make function to display info
+// function getWarriorinfo(){
+
+// }
 
 // TEST log for new Array
-warriorArrayV2.forEach(function (item, index, warriorArrayV2) {
+warriorArray.forEach(function (item, index, warriorArray) {
   console.log(item, index);
 });
 
@@ -134,7 +144,6 @@ for (var k = 0; k < itemButton.length; k++) {
   });
 }
 
-// let characterArray =
 /* TIMER for BATTLE
  var timeleft = 120;
  var downloadTimer = setInterval(function(){
